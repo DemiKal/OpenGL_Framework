d194982ac392eb39f47128156b8a0f972e01391c
DemiKal <DemiKal@users.noreply.github.com>
2020-02-07 22:13:09 +0100

    added libs

--- OpenGL framework/src/GameObject/Components/Model.h
+++ OpenGL framework/src/GameObject/Components/Model.h
@@ -8,6 +8,12 @@ public:
 		PLANE,
 		CUBE
 	};
+	glm::mat4 model;
+	std::vector<MeshNew> meshes;
+	std::string directory;
+	std::vector<Texture2D> textures_loaded;
+	unsigned int shaderIdx;
+
 	//VertexBufferLayout vbl;
 	Model() : model(glm::mat4(1.0f)), meshes(), directory(""), textures_loaded(), shaderIdx(0) {}
 	~Model() = default;
@@ -29,16 +35,7 @@ public:
 	void SetShader(const std::string& shadername);
 	void Draw(const Camera& cam);
 	GPUShader& GetShader() const { return   ShaderManager::getShaderIdx(shaderIdx); }
-	//void CreatePlane();
 	static Model CreateCube();
-
-
-	glm::mat4 model;
-	std::vector<MeshNew> meshes;
-	std::string directory;
-	std::vector<Texture2D> textures_loaded;
-	unsigned int shaderIdx;
-
 	void loadModel(const std::string& path);
 	static Model CreatePlane();
 	void processNode(aiNode *node, const aiScene *scene);
